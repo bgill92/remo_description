8074e3a27bb597520a55220302f75d31371ba259
bgill92 <bilal.gill@picknik.ai>
2023-12-06 18:00:12 -0500

    Updates for ROS 2

---
+++ launch/view_remo.launch.py
@@ -0,0 +1,99 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from launch_param_builder import ParameterBuilder
+
+def launch_setup(context, *args, **kwargs):
+    # Initialize Arguments
+    description_package = LaunchConfiguration("description_package").perform(context)
+    description_file = LaunchConfiguration("description_file").perform(context)
+    gui = LaunchConfiguration("gui").perform(context)
+    prefix = LaunchConfiguration("prefix").perform(context)
+
+    robot_description = (
+        ParameterBuilder(description_package)
+        .xacro_parameter(
+            parameter_name="robot_description",
+            file_path="urdf/remo.urdf.xacro",
+            mappings={
+                "prefix": prefix,
+            },
+        )
+        .to_dict()
+    )
+    
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "remo_description.rviz"]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        condition=IfCondition(gui),
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(gui),
+    )
+
+    nodes = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return nodes
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="remo_description",
+            description="Description package with robot URDF/xacro files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="remo.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gui",
+            default_value="true",
+            description="Start Rviz2 and Joint State Publisher gui automatically \
+        with this launch file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
